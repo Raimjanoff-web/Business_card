d37b2bfea3abe5b697bfc5ba6000d322d9f85e87
Raimjanoff-web <sodiqbek2002@gmail.com>
2023-12-14 16:58:16 +0500

    wrapper is done

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,9 @@
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div className='wrapper d-flex align-items-center justify-content-center'>
+        <div className="item rounded-5 shadow-lg"></div>
+    </div>
+  )
+}
